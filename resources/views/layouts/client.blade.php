00da5bc91b5008f9bf2a4366efa168379cc5991a
itsgitz <anggit.ginanjar@outlook.com>
2021-11-16 15:59:54 +0700

    feat: new layout for all client application pages

--- resources/views/layouts/client.blade.php
+++ resources/views/layouts/client.blade.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minuman Tile - @yield('title')</title>
-    <link href="/css/app.css" rel="stylesheet">
-    <script src="/js/app.js"></script>
+    <title>Minuman Tile - @yield ('title')</title>
+    <link href="{{ mix ('css/app.css') }}" rel="stylesheet">
+    <script defer src="{{ mix ('js/app.js') }}"></script>
 </head>
 <body>
     <div class="container">
-        @yield('content')
+        @include ('shared.client_navigation')
+        @yield ('content')
     </div> 
 </body>
 </html>
