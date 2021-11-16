1f355eea541baff6e326707d6b60ae79625fad44
itsgitz <anggit.ginanjar@outlook.com>
2021-11-16 15:57:05 +0700

    chore: Changed named route for login page

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -15,7 +15,7 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            return route('client_login');
+            return route('client_login_get');
         }
     }
 }
