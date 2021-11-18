980300b83bb00df3aa4ca55f89f25133f40d07a9
itsgitz <anggit.ginanjar@outlook.com>
2021-11-18 09:27:09 +0700

    feat: Add form for save the order from cart. Todo: finish

--- resources/views/client/cart.blade.php
+++ resources/views/client/cart.blade.php
@@ -5,30 +5,36 @@
 <div class="py-3"></div>
 <div class="p-3">
     @if ( !$carts->isEmpty() )
+
     <div class="cart-list">
         <h3 class="fw-light">Keranjang</h3>
 
-        @foreach ($carts as $c)
-        <div class="py-3 border-top border-bottom">
-            <div class="py-1">
-                <span class="fs-5">{{ $c->menu_name }}</span>
-            </div>
-            <div class="py-1">
-                <strong>Rp. {{ number_format( $c->menu_price, 2, ',', '.' ) }}</strong>
-            </div>
-            <div class="py-1">
-                <form action="{{ route('client_order_post') }}" method="post">
-                    @csrf
+        <form action="{{ route('client_order_post') }}" method="post">
+            @csrf
+
+            @foreach ($carts as $c)
+            <div class="cart-item">
+                <div class="py-1"></div>
+                <div class="py-3 border-top border-bottom">
+                    <div class="py-1">
+                        <span class="fs-5">{{ $c->menu_name }}</span>
+                    </div>
+                    <div class="py-1">
+                        <strong>Rp. {{ number_format( $c->menu_price, 2, ',', '.' ) }}</strong>
+                    </div>
+                    <div class="py-1">
                     <input class="form-control w-25" type="number" name="quantity" value="{{ $c->cart_quantity }}">
-                </form>
-                <div class="py-3">
-                    <a class="btn btn-primary" href="{{ route('client_home') }}">Lanjut Memesan</a>
+                    </div>
                 </div>
+                <div class="py-1"></div>
             </div>
-        </div>
-        @endforeach
+            @endforeach
+
+        </form>
     </div> 
+
     @else
+
     <div class="cart-empty py-5">
         <div class="d-flex justify-content-center py-1">
             <h2>Keranjang belanja anda kosong</h2>
@@ -37,6 +43,7 @@
             <a class="btn btn-primary" href="{{ route('client_home') }}">Mulai Memesan</a>
         </div>
     </div>
+
     @endif
 
 </div>
