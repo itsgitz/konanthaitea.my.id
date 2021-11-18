e884444dee478f8cbfe323ecd3fe919fd3472cc9
itsgitz <anggit.ginanjar@outlook.com>
2021-11-18 09:31:51 +0700

    feat: Add layout for admin area

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Area - @yield ('title')</title>
+    <link href="{{ mix ('css/app.css') }}" rel="stylesheet">
+    <script defer src="{{ mix ('js/app.js') }}"></script>
+</head>
+<body>
+    <div class="container-fluid">
+        @include ('shared.admin_navigation')
+
+        @yield ('content')
+    </div> 
+</body>
+</html>
