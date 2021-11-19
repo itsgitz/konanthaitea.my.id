9e1d04db5f2e02be62011ef67624f02eb1b74bdb
itsgitz <anggit.ginanjar@outlook.com>
2021-11-19 11:42:08 +0700

    feat: Add migration command

--- script/run-local.sh
+++ script/run-local.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
 
+echo "[*] Run MariaDB ..."
 ./script/run-mariadb.sh
+
+echo "[*] Run migrations ..."
+sleep 1
+echo "[*] Waiting for database connection ..."
+sleep 10
+php artisan migrate
+php artisan db:seed
+
+echo "[*] Run Laravel web server ..."
 php artisan serve
