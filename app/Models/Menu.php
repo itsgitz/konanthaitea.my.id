aadb72e8b1170334c051e0ca6520116c9e40c1db
itsgitz <anggit.ginanjar@outlook.com>
2021-11-20 06:46:57 +0700

    fix: Removed relation function

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -8,9 +8,4 @@ use Illuminate\Database\Eloquent\Model;
 class Menu extends Model
 {
     use HasFactory;
-
-    /* public function menuStocks() */
-    /* { */
-    /*     return $this->hasMany(MenuStock::class); */
-    /* } */
 }
