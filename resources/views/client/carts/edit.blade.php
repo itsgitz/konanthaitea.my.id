510d14b49d2dca781b36433b4ef4e46f45ed1ab0
itsgitz <anggit.ginanjar@outlook.com>
2021-11-20 06:48:47 +0700

    fix: Use hidden input

--- resources/views/client/carts/edit.blade.php
+++ resources/views/client/carts/edit.blade.php
@@ -5,22 +5,28 @@
 <div class="py-3">
     <div class="cart-edit">
        <h3 class="fw-light">Ubah Item</h3> 
+        <div class="py-1"></div>
         
-        <form action="{{ route('client_cart_update', [ 'cartId' => $cart->id ]) }}" method="post"> 
-            @csrf
-            @method ('PUT')
-            <div class="mb-3">
-                <input class="form-control w-25" type="text" aria-label="readonly" name="menu_name" value="{{ $cart->menu_name }}" readonly>
-            </div>
-            <div class="mb-3">
-                <input class="form-control w-25" type="text" aria-label="readonly" name="menu_price" value="{{ $cart->menu_price }}" readonly> 
-            </div>
-            <div class="mb-3">
-                <input class="form-control w-25" type="number" name="cart_quantity" value="{{ $cart->cart_quantity }}">
-            </div>
-            <a class="btn btn-danger" href="{{ route('client_cart_get') }}">Batal</a>
-            <input class="btn btn-success" type="submit" value="Simpan">
-        </form>
+        <div class="py-3 border-top border-bottom">
+            <form action="{{ route('client_cart_update', [ 'cartId' => $cart->id ]) }}" method="post"> 
+                @csrf
+                @method ('PUT')
+                <div class="mb-3">
+                    <input type="hidden" name="menu_name" value="{{ $cart->menu_name }}">
+                    <h3 class="fw-light">{{ $cart->menu_name }}</h3>
+                </div>
+                <div class="mb-3">
+                    <input type="hidden" name="menu_price" value="{{ $cart->menu_price }}"> 
+                    Rp. {{ number_format( $cart->menu_price, 2, ',', '.' ) }}
+                </div>
+                <div class="mb-3">
+                    <label class="label" for="quantity">Jumlah</label>
+                    <input id="quantity" class="form-control w-25" type="number" name="cart_quantity" value="{{ $cart->cart_quantity }}">
+                </div>
+                <a class="btn btn-danger" href="{{ route('client_cart_get') }}">Batal</a>
+                <input class="btn btn-success" type="submit" value="Simpan">
+            </form>
+        </div>
     </div>
 </div>
 @endsection
