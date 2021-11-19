39523f0dc57c2bd6cb8f6a0f71b22107ff6c333f
itsgitz <anggit.ginanjar@outlook.com>
2021-11-19 11:37:40 +0700

    feat: Content

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,1 +1,14 @@
 @extends ('layouts.client')
+
+@section ('title', 'Tentang aplikasi ini')
+
+@section ('content')
+<div class="py-3">
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title fw-light d-flex justify-content-center">Tentang Aplikasi</h4>
+            <span class="card-text fw-light d-flex justify-content-center">Minuman versi 0.1</span>
+        </div>
+    </div>
+</div>
+@endsection
