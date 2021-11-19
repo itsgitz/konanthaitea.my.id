aa808216016cf9bb16f65814e4d6fbabda1de395
itsgitz <anggit.ginanjar@outlook.com>
2021-11-19 11:40:43 +0700

    feat: Add menu_price input value. Updated content.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,25 +5,27 @@
 
 @include ('shared.message')
 
-<div class="py-3"></div>
-<div class="row">
-    @foreach ($menu as $m) 
-    <div class="col-md-3">
-        <div class="card shadow mb-5 bg-body">
-            <div class="card-body">
-                <h1 class="card-title">{{ $m->name }}</h1>
-                <p class="card-text">Rp. {{ number_format( $m->price, 2, ',', '.' ) }}</p>
-                <p class="card-text">{{ $m->status }}</p>
-                <div class="order-link">
-                    <form action="{{ route('client_cart_post') }}" method="post">
-                        @csrf
-                        <input type="hidden" name="menu_id" value="{{ $m->id }}">
-                        <input class="btn btn-outline-primary rounded-pill" type="submit" value="Tambah">
-                    </form>
+<div class="py-3">
+    <div class="row">
+        @foreach ($menu as $m) 
+        <div class="col-md-3">
+            <div class="card shadow mb-5 bg-body">
+                <div class="card-body">
+                    <h1 class="card-title">{{ $m->name }}</h1>
+                    <p class="card-text">Rp. {{ number_format( $m->price, 2, ',', '.' ) }}</p>
+                    <p class="card-text">{{ $m->status }}</p>
+                    <div class="order-link">
+                        <form action="{{ route('client_cart_post') }}" method="post">
+                            @csrf
+                            <input type="hidden" name="menu_id" value="{{ $m->id }}">
+                            <input type="hidden" name="menu_price" value="{{ $m->price }}">
+                            <input class="btn btn-outline-primary rounded-pill" type="submit" value="Tambah">
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
 </div>
 @endsection
