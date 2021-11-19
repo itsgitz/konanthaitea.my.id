4a07118f3c578b344212c89707d54be75b6e2b8e
itsgitz <anggit.ginanjar@outlook.com>
2021-11-19 11:41:36 +0700

    feat: Add routes for update and delete cart

--- routes/cart.php
+++ routes/cart.php
@@ -11,5 +11,14 @@ Route::get('/minuman/cart', [\App\Http\Controllers\CartController::class, 'index
 Route::post('/minuman/cart', [\App\Http\Controllers\CartController::class, 'store'])
     ->name('client_cart_post');
 
-Route::get('/minuman/cart/delete', [\App\Http\Controllers\CartController::class, 'delete'])
-    ->name('client_cart_delete');
+Route::get('/minuman/cart/delete/{cartId}', [\App\Http\Controllers\CartController::class, 'delete'])
+    ->name('client_cart_delete')
+    ->middleware('auth');
+
+Route::get('/minuman/cart/edit/{cartId}', [\App\Http\Controllers\CartController::class, 'edit'])
+    ->name('client_cart_edit')
+    ->middleware('auth');
+
+Route::put('/minuman/cart/edit/{cartId}', [\App\Http\Controllers\CartController::class, 'update'])
+    ->name('client_cart_update')
+    ->middleware('auth');
