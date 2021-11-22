97eb79e64b3999c6f654d778eede513a03e5eab5
itsgitz <anggit.ginanjar@outlook.com>
2021-11-22 14:28:19 +0700

    style: Badges

--- resources/views/client/orders/index.blade.php
+++ resources/views/client/orders/index.blade.php
@@ -11,56 +11,62 @@
                 <div class="d-inline card-text px-3">
                     <i class="fa fa-shopping-bag text-success"></i>
                 </div> 
-                <div class="d-inline card-text">
-                    <span class="fw-bold">{{ date('j M Y', strtotime( $o['created_at'] )) }}</span>
+                <div class="d-inline card-text fw-bold">
+                    {{ date('j M Y', strtotime( $o['created_at'] )) }}
                 </div>
-                <div class="d-inline card-text">
-                    <span>{{ $o['payment_status'] }}</span>
+                <div class="d-inline px-1"></div>
+                <div class="d-inline card-text badge bg-danger">
+                    {{ $o['payment_status'] }}
                 </div>
-                <div class="d-inline card-text">
-                    <span>Order ID: #{{ $o['id'] }}</span>
+                <div class="d-inline px-1"></div>
+                <div class="card-text d-inline badge bg-warning">
+                    {{ $o['delivery_status'] }}
                 </div>
-
+                <div class="d-inline px-1"></div>
+                <div class="d-inline card-text fw-bold">
+                    Order ID: #{{ $o['id'] }}
+                </div>
+ 
                 {{-- Ordered Carts --}}
-                <div class="py-2"></div>
+                <div class="py-1"></div>
                 <div class="card-text">
                     <ul class="list-group list-group-flush">
                     @foreach ($o['carts'] as $cart)
                         <li class="list-group-item">
                             <h5 class="card-title">{{ $cart['menu_name'] }}</h5>
-                            <span class="card-text fw-light">
-                               {{ $cart['quantity'] }} minuman x Rp. {{ number_format( $cart['subtotal_amount'], 2, ',', '.') }} 
-                            </span>
+                            <div class="card-text fw-light">
+                               {{ $cart['quantity'] }} minuman x Rp. {{ number_format( $cart['menu_price'], 2, ',', '.' ) }} 
+                            </div>
+                            <div class="card-text fw-light">
+                                Subtotal Harga Rp. {{ number_format( $cart['subtotal_amount'], 2, ',', '.') }}
+                            </div>
                         </li>
                     @endforeach
                     </ul>
-                </div>                
+                </div>
+                <div class="py-1"></div>
                 {{-- Ordered Carts --}}
 
                 {{-- Total Amount --}}
-                <div class="py-1"></div>
-                <div class="card-text border-top px-3 py-3">
+                <div class="card-text border-top p-3">
                     <h5 class="card-title">Total Harga</h5>
-                    <span class="card-text fw-light">Rp. {{ number_format( $o['total_amount'], 2, ',', '.' ) }}</span>
+                    <div class="card-text fw-light">Rp. {{ number_format( $o['total_amount'], 2, ',', '.' ) }}</div>
                 </div>
-                <div class="py-1"></div>
                 {{-- Total Amount --}}
 
                 {{-- Show Details --}}
-                <div class="py-1"></div>
-                <div class="card-text px-3">
-                    <a class="text-success text-decoration-none fw-bold" href="{{ route('client_order_show', [ 'id' => $o['id'] ]) }}">Lihat Detail Transaksi</a>
+                <div class="card-text px-2">
+                    <a class="btn btn-sm text-success fw-light" href="{{ route('client_order_show', ['id' => $o['id']]) }}">Lihat Detail Transaksi</a>
                 </div>
-                <div class="py-1"></div>
                 {{-- Show Details --}}
             </div>
         </div>
         <div class="py-1"></div>
         @endforeach
 
-        <div class="py-3">
-            <a class="btn btn-primary" href="{{ route('client_home') }}">Lanjut Belanja</a>
-            <a class="btn btn-primary" href="{{ route('client_cart_get') }}">Lihat Keranjang</a>
+        <div class="py-4">
+            <a class="btn btn-primary" href="{{ route('client_home') }}">Belanja</a>
+            <a class="btn btn-primary" href="{{ route('client_cart_get') }}">Keranjang</a>
         </div>
     @else
         <div class="order-empty py-5">
