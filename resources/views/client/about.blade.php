80345620ae497cf60dbd8cb69187d07d5e0219b3
itsgitz <anggit.ginanjar@outlook.com>
2021-11-22 14:27:45 +0700

    chore: Updated style, centered box

--- resources/views/client/about.blade.php
+++ resources/views/client/about.blade.php
@@ -4,10 +4,13 @@
 
 @section ('content')
 <div class="py-3">
-    <div class="card">
-        <div class="card-body">
-            <h4 class="card-title fw-light d-flex justify-content-center">Tentang Aplikasi</h4>
-            <span class="card-text fw-light d-flex justify-content-center">Minuman versi 0.1</span>
+    <div class="py-3"></div>
+    <div class="d-flex justify-content-center">
+        <div class="card w-50">
+            <div class="card-body">
+                <h4 class="card-title fw-light d-flex justify-content-center">Tentang Aplikasi</h4>
+                <span class="card-text fw-light d-flex justify-content-center">Minuman versi 0.1</span>
+            </div>
         </div>
     </div>
 </div>
