6b0d94cd76c70e3a318427a05d28e4638089b206
itsgitz <anggit.ginanjar@outlook.com>
2021-11-22 14:29:54 +0700

    style: Updated

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,3 +1,3 @@
-<footer class="container footer mt-auto p-4 bg-white border-top">
-    <span class="fw-light">&copy; Aplikasi Minuman v0.1 2021</span> 
+<footer class="footer mt-auto p-4 bg-white border-top">
+    <div class="container"><span class="fw-light small">&copy; Aplikasi Minuman v0.1 2021</span></div> 
 </footer>
