489ba1dfad01a478f218b7e54c4097e22402803a
itsgitz <anggit.ginanjar@outlook.com>
2021-11-24 12:47:27 +0700

    feat: Add error messages

--- resources/views/admin/accounts/clients/edit.blade.php
+++ resources/views/admin/accounts/clients/edit.blade.php
@@ -1,9 +1,9 @@
 @extends ('layouts.admin')
-@section ('title', 'Edit Clients')
+@section ('title', 'Edit Client ' . $client->name)
 
 @section ('content')
 <div class="py-3">
-    <h5>Edit Client</h5>
+    <h5>Edit Client - {{ $client->name }}</h5>
 
     @include ('shared.message')
 
@@ -12,13 +12,28 @@
         @method('PUT')
 
         <div class="mb-3 col-md-4">
-            <input class="form-control" type="text" name="name" value="{{ $client->name }}" placeholder="Nama">
+            <input class="form-control" type="text" name="name" value="{{ $client->name }}" placeholder="Nama" required>
+            @error ('name')
+            <div>
+                <span class="text-danger fw-light"><small>{{ $message }}</small></span>
+            </div>
+            @enderror
         </div>
         <div class="mb-3 col-md-4">
-            <input class="form-control" type="text" name="email" value="{{ $client->email }}" placeholder="Nama">
+            <input class="form-control" type="text" name="email" value="{{ $client->email }}" placeholder="Email" required>
+            @error ('email')
+            <div>
+                <span class="text-danger fw-light"><small>{{ $message }}</small></span>
+            </div>
+            @enderror
         </div>
         <div class="mb-3 col-md-4">
             <input class="form-control" type="password" name="password" placeholder="Password">
+            @error ('password')
+            <div>
+                <span class="text-danger fw-light"><small>{{ $message }}</small></span>
+            </div>
+            @enderror
         </div>
         <a class="btn btn-danger" href="{{ route('admin_clients_get') }}">Kembali</a>
         <input class="btn btn-primary" type="submit" value="Simpan">
