5542b083e5ee9e905a796e2f4ba4ce89fb0dbcdd
itsgitz <anggit.ginanjar@outlook.com>
2021-11-24 10:35:47 +0700

    style: Add footer, changed container style

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,10 +7,11 @@
     <link href="{{ mix ('css/app.css') }}" rel="stylesheet">
     <script defer src="{{ mix ('js/app.js') }}"></script>
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
     @include ('shared.admin_navigation')
-    <div class="container-fluid">
+    <div class="container">
         @yield ('content')
     </div> 
+    @include ('shared.footer')
 </body>
 </html>
