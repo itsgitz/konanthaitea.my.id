f832712009d2c68f38e372f1fa14b356ade5be93
itsgitz <anggit.ginanjar@outlook.com>
2021-11-24 12:48:01 +0700

    style: Updated

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -8,10 +8,13 @@
     <div class="py-2"></div>
     <div class="row gy-3">
         <div class="col-md">
-            <a href="{{ route('client_home') }}">
-                <div class="card">
+            <a class="text-decoration-none" href="{{ route('client_home') }}">
+                <div class="card btn btn-outline-success">
                     <div class="card-body">
-                        <div class="card-text">
+                        <div class="card-text d-flex justify-content-center">
+                            <i class="fas fa-shopping-cart display-3"></i>
+                        </div>
+                        <div class="card-text d-flex justify-content-center py-3">
                             Client Area
                         </div>
                     </div>
@@ -19,10 +22,13 @@
             </a>
         </div>
         <div class="col-md">
-            <a href="{{ route('admin_orders_get') }}">
-                <div class="card">
+            <a class="text-decoration-none" href="{{ route('admin_orders_get') }}">
+                <div class="card btn btn-outline-primary">
                     <div class="card-body">
-                        <div class="card-text">
+                        <div class="card-text d-flex justify-content-center">
+                            <i class="fas fa-clipboard-list display-3"></i>
+                        </div>
+                        <div class="card-text d-flex justify-content-center py-3">
                             Orders
                         </div>
                     </div>
@@ -30,10 +36,13 @@
             </a>
         </div>
        <div class="col-md">
-            <a href="{{ route('admin_stocks_get') }}">
-                <div class="card">
+            <a class="text-decoration-none" href="{{ route('admin_stocks_get') }}">
+                <div class="card btn btn-outline-warning">
                     <div class="card-body">
-                        <div class="card-text">
+                        <div class="card-text d-flex justify-content-center">
+                            <i class="fas fa-tags display-3"></i>
+                        </div>
+                        <div class="card-text d-flex justify-content-center py-3">
                             Stocks
                         </div>
                     </div>
@@ -41,10 +50,13 @@
             </a>
         </div> 
         <div class="col-md">
-            <a href="{{ route('admin_menus_get') }}">
-                <div class="card">
+            <a class="text-decoration-none" href="{{ route('admin_menus_get') }}">
+                <div class="card btn btn-outline-danger">
                     <div class="card-body">
-                        <div class="card-text">
+                        <div class="card-text d-flex justify-content-center">
+                            <i class="fas fa-wine-bottle display-3"></i>
+                        </div>
+                        <div class="card-text d-flex justify-content-center py-3">
                             Menu
                         </div>
                     </div>
