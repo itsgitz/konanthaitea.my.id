2d8e2b255e2bb564e1a1ae69d0861a08f271d9e2
itsgitz <anggit.ginanjar@outlook.com>
2021-11-24 10:37:38 +0700

    feat: Clients management routes

--- routes/client.php
+++ routes/client.php
@@ -3,10 +3,29 @@
 use Illuminate\Support\Facades\Route;
 
 //
-//Client area route
+//Client area routes
 //
 Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])
     ->name('client_home');
 
 Route::get('/about', [\App\Http\Controllers\HomeController::class, 'about'])
     ->name('client_about');
+
+//Client user management routes
+Route::get('/admin/clients', [\App\Http\Controllers\ClientsController::class, 'index'])
+    ->name('admin_clients_get');
+
+Route::get('/admin/clients/add', [\App\Http\Controllers\ClientsController::class, 'create'])
+    ->name('admin_clients_add_get');
+
+Route::post('/admin/clients/add', [\App\Http\Controllers\ClientsController::class, 'store'])
+    ->name('admin_clients_add_post');
+
+Route::get('/admin/clients/edit/{id}', [\App\Http\Controllers\ClientsController::class, 'edit'])
+    ->name('admin_clients_edit_get');
+
+Route::put('/admin/clients/edit/{id}', [\App\Http\Controllers\ClientsController::class, 'update'])
+    ->name('admin_clients_edit_put');
+
+Route::get('/admin/clients/delete/{id}', [\App\Http\Controllers\ClientsController::class, 'delete'])
+    ->name('admin_clients_delete_get');
