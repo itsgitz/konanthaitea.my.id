42fbc409a047eed0a43383414c396ae54bb96899
itsgitz <anggit.ginanjar@outlook.com>
2021-11-24 12:47:01 +0700

    feat: Add features for create, update, and delete admin user

--- resources/views/admin/accounts/admins/create.blade.php
+++ resources/views/admin/accounts/admins/create.blade.php
@@ -1,17 +1,14 @@
 @extends ('layouts.admin')
-@section ('title', 'Edit Admin ' . $admin->name)
+@section ('title', 'Tambah Admin')
 
 @section ('content')
 <div class="py-3">
-    <h5>Edit Admin - {{ $admin->name }}</h5>
+    <h5>Tambah Admin</h5>
 
-    @include ('shared.message')
-    <form action="{{ route('admin_accounts_edit_put', [ 'id' => $admin->id ]) }}" method="post">
+    <form action="{{ route('admin_accounts_add_post') }}" method="post">
         @csrf
-        @method('PUT')
-
         <div class="mb-3 col-md-4">
-            <input class="form-control" type="text" name="name" value="{{ $admin->name }}" placeholder="Nama" required>
+            <input class="form-control" type="text" name="name" placeholder="Nama" value="{{ old('name') }}" required>
             @error ('name')
             <div>
                 <span class="text-danger fw-light"><small>{{ $message }}</small></span>
@@ -19,7 +16,7 @@
             @enderror
         </div>
         <div class="mb-3 col-md-4">
-            <input class="form-control" type="email" name="email" value="{{ $admin->email }}" placeholder="Email" required>
+            <input class="form-control" type="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
             @error ('email')
             <div>
                 <span class="text-danger fw-light"><small>{{ $message }}</small></span>
@@ -27,15 +24,15 @@
             @enderror
         </div>
         <div class="mb-3 col-md-4">
-            <input class="form-control" type="password" name="password" placeholder="Password">
+            <input class="form-control" type="password" name="password" placeholder="Password" required>
             @error ('password')
             <div>
                 <span class="text-danger fw-light"><small>{{ $message }}</small></span>
             </div>
             @enderror
         </div>
-        
-        <a class="btn btn-danger" href="{{ route('admin_accounts_get') }}">Kembali</a>
+
+        <a class="btn btn-danger" href="{{ route('admin_accounts_get') }}">Batal</a>
         <input class="btn btn-primary" type="submit" value="Simpan">
     </form>
 </div>
--- resources/views/admin/accounts/admins/index.blade.php
+++ resources/views/admin/accounts/admins/index.blade.php
@@ -1,5 +1,83 @@
 @extends ('layouts.admin')
-@section ('title', 'Clients')
+@section ('title', 'Admins Management')
 
 @section ('content')
+<div class="py-3">
+    <h5>Admins Management</h5>
+
+    @include ('shared.message')
+
+    <div class="py-2">
+        <a class="btn btn-primary btn-sm" href="{{ route('admin_accounts_add_get') }}">
+            Tambah Admin
+        </a>
+    </div>
+    <table class="table table-hover">
+        <thead>
+            <th scope="col">Nama</th>
+            <th scope="col">Email</th>
+            <th scope="col">Dibuat Tanggal</th>
+            <th scope="col">#</th>
+        </thead>
+        
+        @foreach ($admins as $a)
+        <tr>
+            <td class="fw-light">{{ $a->name }}</td>
+            <td class="fw-light">{{ $a->email }}</td>
+            <td class="fw-light">{{ $a->created_at }}</td>
+            <td>
+                <a class="btn btn-warning" href="{{ route('admin_accounts_edit_get', [ 'id' => $a->id ]) }}">
+                    Edit
+                </a>
+                <span class="px-2"></span>
+                <a
+                    class="btn btn-danger"
+                    href="#"
+                    data-bs-toggle="modal"
+                    data-bs-target="#remove-admin-modal"
+                    data-admin-name="{{ $a->name }}"
+                    data-remove-admin-link="{{ route('admin_accounts_delete_get', ['id' => $a->id]) }}"
+                    onclick="getAdmin(this)"
+                >
+                    Remove
+                </a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {{-- Modal --}}
+    <div id="remove-admin-modal" class="modal fade fw-light" tabindex="-1" aria-labelledby="remove-admin-modal-label">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 id="remove-admin-modal-label" class="modal-title">Hapus Admins</h5>
+                    <button class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    Apakah anda yakin ingin menghapus admin <span id="admin-name" class="fw-bold"></span>?
+                </div>
+                <div class="modal-footer">
+                    <button class="btn btn-primary" data-bs-dismiss="modal">Batal</button>
+                    <a id="remove-admin-button" class="btn btn-danger" href="">Hapus</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- Modal --}}
+
+    {{-- Modal Script --}}
+    <script>
+        function getAdmin(el) {
+            let adminName = el.dataset.adminName,
+                removeAdminLink = el.dataset.removeAdminLink,
+                showAdminName = document.querySelector('#admin-name'),
+                removeAdminButton = document.querySelector('#remove-admin-button');
+
+            showAdminName.innerHTML = adminName;
+            removeAdminButton.setAttribute('href', removeAdminLink);
+        }
+    </script>
+    {{-- Modal Script --}}
+</div>
 @endsection
