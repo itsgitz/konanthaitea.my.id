6b66b753b1a48286544500ea390a48b0a3ecf86d
itsgitz <anggit.ginanjar@outlook.com>
2021-11-24 10:37:21 +0700

    feat: Admin management routes

--- routes/admin.php
+++ routes/admin.php
@@ -31,3 +31,22 @@ Route::get('/admin/orders/{id}', [\App\Http\Controllers\OrdersController::class,
 
 Route::put('/admin/orders/process/{id}', [\App\Http\Controllers\OrdersController::class, 'adminProcess'])
     ->name('admin_orders_process');
+
+//Admin user management routes
+Route::get('/admin/accounts', [\App\Http\Controllers\AdminsController::class, 'account'])
+    ->name('admin_accounts_get');
+
+Route::get('/admin/accounts/add', [\App\Http\Controllers\AdminsController::class, 'create'])
+    ->name('admin_accounts_add_get');
+
+Route::post('/admin/accounts/add', [\App\Http\Controllers\AdminsController::class, 'store'])
+    ->name('admin_accounts_add_post');
+
+Route::get('/admin/accounts/edit/{id}', [\App\Http\Controllers\AdminsController::class, 'edit'])
+    ->name('admin_accounts_edit_get');
+
+Route::put('/admin/accounts/edit/{id}', [\App\Http\Controllers\AdminsController::class, 'update'])
+    ->name('admin_accounts_edit_put');
+
+Route::get('/admin/accounts/delete/{id}', [\App\Http\Controllers\AdminsController::class, 'delete'])
+    ->name('admin_accounts_delete_get');
