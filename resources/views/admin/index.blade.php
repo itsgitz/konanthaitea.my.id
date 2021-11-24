5a7bc413bda53ddb4de35fd532ea018c03e294a4
itsgitz <anggit.ginanjar@outlook.com>
2021-11-24 10:32:12 +0700

    feat: Add dashboard content

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -4,5 +4,53 @@
 @section ('content')
 <div class="py-3">
     <h5>Dashboard</h5>
+
+    <div class="py-2"></div>
+    <div class="row gy-3">
+        <div class="col-md">
+            <a href="{{ route('client_home') }}">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-text">
+                            Client Area
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="col-md">
+            <a href="{{ route('admin_orders_get') }}">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-text">
+                            Orders
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div>
+       <div class="col-md">
+            <a href="{{ route('admin_stocks_get') }}">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-text">
+                            Stocks
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div> 
+        <div class="col-md">
+            <a href="{{ route('admin_menus_get') }}">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="card-text">
+                            Menu
+                        </div>
+                    </div>
+                </div>
+            </a>
+        </div> 
+    </div>
 </div>
 @endsection
