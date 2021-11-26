0df910b97883d8ead999b31eb01a8dd644d0f99b
itsgitz <anggit.ginanjar@outlook.com>
2021-11-26 11:09:24 +0700

    chore: Empty data messages

--- resources/views/admin/menus/index.blade.php
+++ resources/views/admin/menus/index.blade.php
@@ -5,32 +5,39 @@
 <div class="py-3">
     <h5>Menu Management</h5>
 
-    <div class="py-2">
+    @include ('shared.message')
+    <div class="pt-2 pb-3">
         <a class="btn btn-primary btn-sm" href="{{ route('admin_menu_add_get') }}">Tambah Menu</a>
     </div>
 
-    <table class="table">
-        <th>ID</th>
-        <th>Name</th>
-        <th>Price</th>
-        <th>Status</th>
-        <th>Quantity</th>
-        <th>Added At</th>
-        <th>Action</th>
-        
-        @foreach ($menus as $m)
-        <tr>
-            <td>{{ $m->id }}</td>
-            <td>{{ $m->name }}</td>
-            <td>Rp. {{ number_format( $m->price, 2, ',', '.' ) }}</td>
-            <td>{{ $m->status }}</td>
-            <td>{{ $m->quantity }}</td>
-            <td>{{ $m->created_at }}</td>
-            <td>
-                <a class="btn btn-sm btn-success" href="{{ route('admin_menu_show_get', [ 'id' => $m->id ]) }}">Lihat Resep</a>
-            </td>
-        </tr>
-        @endforeach
+    <table class="table table-hover">
+        <thead>
+            <th scope="col">Harga</th>
+            <th scope="col">Status</th>
+            <th scope="col">Jumlah</th>
+            <th scope="col">Ditambahkan Tanggal</th>
+            <th scope="col">#</th>
+        </thead>
+
+        @if ($menus->isNotEmpty())
+            @foreach ($menus as $m)
+            <tr>
+                <td>{{ $m->id }}</td>
+                <td>{{ $m->name }}</td>
+                <td>Rp. {{ number_format( $m->price, 2, ',', '.' ) }}</td>
+                <td>{{ $m->status }}</td>
+                <td>{{ $m->quantity }}</td>
+                <td>{{ $m->created_at }}</td>
+                <td>
+                    <a class="btn btn-success btn-sm" href="{{ route('admin_menu_show_get', [ 'id' => $m->id ]) }}">Lihat Resep</a>
+                </td>
+            </tr>
+            @endforeach
+        @else
+            <tr>
+                <td class="fw-light text-center" colspan="6">Data menu kosong</td>
+            </tr>
+        @endif
     </table>
 </div>
 @endsection
