08218ad1207218002f99d95ef542bfb10480d733
itsgitz <anggit.ginanjar@outlook.com>
2021-11-29 22:01:15 +0700

    feat: Imported main scripts

--- resources/js/menus/main.js
+++ resources/js/menus/main.js
@@ -1,21 +1,7 @@
-export function editMenuQuantity() {
-  let editMenu = document.querySelector('#edit-menu');
+import { setStatusClass } from './index';
+import { editMenuQuantity } from './edit';
 
-  if (editMenu) {
-    //<input id="edit-quantity-input" class="form-control" type="number" min="1">
-    let addMenuQuantityButton = document.querySelector('#add-menu-quantity-button');
-    let reduceMenuQuantityButton = document.querySelector('#reduce-menu-quantity-button');
-    let editAdd = document.querySelector('#edit-add');
-    let editReduce = document.querySelector('#edit-reduce');
-
-    addMenuQuantityButton.onclick = function() {
-      editAdd.classList.remove('d-none');
-      editReduce.classList.add('d-none');
-    }
-
-    reduceMenuQuantityButton.onclick = function() {
-      editReduce.classList.remove('d-none');
-      editAdd.classList.add('d-none');
-    }
-  }
+export function runMenu() {
+  editMenuQuantity();
+  setStatusClass();
 }
