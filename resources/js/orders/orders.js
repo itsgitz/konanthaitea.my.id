dce9d1b6805c47c389ae0b6072ac21517fe46956
itsgitz <anggit.ginanjar@outlook.com>
2021-11-29 22:01:40 +0700

    chore: Renamed

--- resources/js/orders/orders.js
+++ resources/js/orders/orders.js
@@ -1,6 +1,6 @@
-import { clientOrdersMain } from './client/main';
+import { clientOrdersMain } from './client/index';
 import { clientShowOrder } from './client/show';
-import { adminOrdersMain } from './admin/main';
+import { adminOrdersMain } from './admin/index';
 import { adminShowOrder } from './admin/show';
 
 export function runOrders() {
