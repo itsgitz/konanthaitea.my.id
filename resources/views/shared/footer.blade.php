642fadb1e05e1c987ae87cb6d0b89ba2f8e9ed46
itsgitz <anggit.ginanjar@outlook.com>
2022-01-04 17:07:39 +0700

    fix: Footer

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -1,3 +1,3 @@
 <footer class="footer mt-auto p-4 bg-white border-top">
-    <div class="container"><span class="fw-light small">&copy; Aplikasi Minuman v0.1 2021</span></div> 
+    <div class="container"><span class="fw-light small">&copy; Aplikasi Konan Thai Tea v0.1 2021</span></div>
 </footer>
