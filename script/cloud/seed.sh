8bcaf91d356cf3f88224cfeb06bbe14ed5ffc7a5
itsgitz <anggit.ginanjar@outlook.com>
2022-01-04 17:42:35 +0700

    fix: Volume Path

--- script/cloud/seed.sh
+++ script/cloud/seed.sh
@@ -3,6 +3,8 @@
 docker-compose \
     -f cloud.yaml \
     -p cloud-minuman_tile_itsgitz_com \
+    -v "$(pwd):/var/www/minuman-tile.itsgitz.com"
+    -w /var/www/minuman-tile.itsgitz.com
     exec \
     web \
     php artisan db:seed
