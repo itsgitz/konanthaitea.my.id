156f02f23bd460c094413b6cf5259c60eb4edb8a
itsgitz <anggit.ginanjar@outlook.com>
2022-01-04 17:04:13 +0700

    fix: header title

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minuman Tile - @yield ('title')</title>
+    <title>Konan Thai Tea - @yield ('title')</title>
     <link href="{{ mix ('css/app.css') }}" rel="stylesheet">
     <script defer src="{{ mix ('js/app.js') }}"></script>
 </head>
