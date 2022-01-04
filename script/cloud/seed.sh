3b144171c6169e45b4cf08d19d120aadc19b946d
itsgitz <anggit.ginanjar@outlook.com>
2022-01-04 17:43:58 +0700

    fix: Multiple line for seed.sh

--- script/cloud/seed.sh
+++ script/cloud/seed.sh
@@ -3,8 +3,8 @@
 docker-compose \
     -f cloud.yaml \
     -p cloud-minuman_tile_itsgitz_com \
-    -v "$(pwd):/var/www/minuman-tile.itsgitz.com"
-    -w /var/www/minuman-tile.itsgitz.com
+    -v "$(pwd):/var/www/minuman-tile.itsgitz.com" \
+    -w /var/www/minuman-tile.itsgitz.com \
     exec \
     web \
     php artisan db:seed
