455cd83160cc927933d23e8fc60c9cb0cbb97f52
itsgitz <anggit.ginanjar@outlook.com>
2022-01-17 12:24:16 +0700

    chore: Add breaks

--- resources/views/layouts/exports_pdf.blade.php
+++ resources/views/layouts/exports_pdf.blade.php
@@ -66,6 +66,8 @@
             </div>
         </div>
         <div class="break"></div>
+        <div class="break"></div>
+        <div class="break"></div>
         @yield ('content')
     </div>
 </body>
